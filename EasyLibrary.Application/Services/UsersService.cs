911b4d94ef2b05fcab0032f476af7854869d33c9
wackyEvidence <nizapogiti@gmail.com>
2024-05-12 17:44:53 +0500

    Application layer set up

---
+++ EasyLibrary.Application/Services/UsersService.cs
@@ -0,0 +1,36 @@
+﻿using EasyLibrary.Core.Models;
+using EasyLibrary.DataAccess.Repositories;
+
+namespace EasyLibrary.Application.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersService(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _usersRepository.Get();
+        }
+
+        public async Task<Guid> CreateUser(User user)
+        {
+            return await _usersRepository.Create(user);
+        }
+
+        public async Task<Guid> UpdateBook(Guid id, string name, string surname, string patronymic, string passportNumber,
+            string passportSeries, DateOnly birthDate, string? email, string phoneNumber, bool isAdmin)
+        {
+            return await _usersRepository.Update(id, name, surname, patronymic, passportNumber, passportSeries, birthDate, email, phoneNumber, isAdmin);
+        }
+
+        public async Task<Guid> DeleteBook(Guid id)
+        {
+            return await _usersRepository.Delete(id);
+        }
+    }
+}
