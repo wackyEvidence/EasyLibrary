08bfa3d49a234471b1ffd79490384d63f18f63f8
wackyEvidence <nizapogiti@gmail.com>
2024-05-20 20:49:48 +0500

    about and footer components added

--- easy-library-ui/src/App.js
+++ easy-library-ui/src/App.js
@@ -1,13 +1,20 @@
 import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Footer from "./Components/Footer/Footer";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-
     <div className="App">
       <Navbar></Navbar>
-      <Contact></Contact>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
