b071ff2b01849a95b48efe92d8f90ed160ffb97c
wackyEvidence <nizapogiti@gmail.com>
2024-06-19 14:40:21 +0500

    book type mapper fix x2

--- EasyLibrary.DataAccess/Mappers/BookTypeMapper.cs
+++ EasyLibrary.DataAccess/Mappers/BookTypeMapper.cs
@@ -8,19 +8,16 @@ namespace EasyLibrary.DataAccess.Mappers
     {
         private readonly Lazy<IMapper<PublishingHouseEntity, PublishingHouse>> _publishingHouseMapper;
         private readonly Lazy<IMapper<BookSeriesEntity, BookSeries>> _bookSeriesMapper;
-        private readonly Lazy<IMapper<BookCopyEntity, BookCopy>> _bookCopyMapper;
         private readonly Lazy<IMapper<BookAuthorEntity, BookAuthor>> _bookAuthorMapper;
 
         public BookTypeMapper(
             Lazy<IMapper<PublishingHouseEntity, PublishingHouse>> publishingHouseMapper, 
             Lazy<IMapper<BookSeriesEntity, BookSeries>> bookSeriesMapper,
-            Lazy<IMapper<BookCopyEntity, BookCopy>> bookCopyMapper, 
             Lazy<IMapper<BookAuthorEntity, BookAuthor>> bookAuthorMapper
             )
         {
             _publishingHouseMapper = publishingHouseMapper;
             _bookSeriesMapper = bookSeriesMapper;
-            _bookCopyMapper = bookCopyMapper;
             _bookAuthorMapper = bookAuthorMapper;
         }
 
